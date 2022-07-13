763535fc718712d0fba993e604e9e00f191716d9
Stanislav Drehval <stasdre@gmail.com>
2022-07-13 11:15:18 +0300

    add setupIonicReact

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
+import {setupIonicReact} from "@ionic/react";
+
+setupIonicReact({
+    swipeBackEnabled: false,
+});
+
+
 export { default as Segment } from './lib/Segment';
 export * from './lib/Segment';
